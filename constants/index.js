384b3c440481fef5ae5c7c24bb0123c3292b3b0d
MarkusEicher <markuseicher@outlook.com>
2023-04-02 23:25:18 +0200

    Add constants index.js and deploy.js

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xabCD8bC2E95ADe0c3575e6079a05718a2d519d54";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
